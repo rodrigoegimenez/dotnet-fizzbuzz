a5284ff58ea1474687064c0eae51c83fc8eb4180
Rodrigo Gimenez <rodrigog83@gmail.com>
2022-03-25 22:29:24 +0100

    Refactor service

--- FizzBuzzService/FizzBuzzService.cs
+++ FizzBuzzService/FizzBuzzService.cs
@@ -3,15 +3,7 @@ public class FizzBuzzService
 {
     public string Of(int number)
     {
-        if (number == 2)
-        {
-            return "2";
-        }
-        if (number == 4)
-        {
-            return "4";
-        }
-        return "1";
+        return number.ToString();
     }
 
 }
