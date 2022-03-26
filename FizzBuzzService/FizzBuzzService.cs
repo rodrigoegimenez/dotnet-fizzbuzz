6bcb5c822d33520d9733f6db3f35da6114b8fb10
Rodrigo Gimenez <rodrigog83@gmail.com>
2022-03-26 08:48:33 +0100

    Refactor

--- FizzBuzzService/FizzBuzzService.cs
+++ FizzBuzzService/FizzBuzzService.cs
@@ -3,7 +3,7 @@ public class FizzBuzzService
 {
     public string Of(int number)
     {
-        if (number == 15 || number == 30 || number == 45)
+        if (number % 15 == 0)
         {
             return "FizzBuzz";
         }
