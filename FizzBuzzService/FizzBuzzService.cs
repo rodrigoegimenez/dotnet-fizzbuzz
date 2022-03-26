cff0b9bd2e211d73e9564b69de4bfbfcf64fc43d
Rodrigo Gimenez <rodrigog83@gmail.com>
2022-03-26 09:02:00 +0100

    Create IsFizzBuzz function

--- FizzBuzzService/FizzBuzzService.cs
+++ FizzBuzzService/FizzBuzzService.cs
@@ -3,7 +3,7 @@ public class FizzBuzzService
 {
     public string Of(int number)
     {
-        if (IsFizz(number) && IsBuzz(number))
+        if (IsFizzBuzz(number))
         {
             return "FizzBuzz";
         }
@@ -20,5 +20,5 @@ public class FizzBuzzService
 
     private bool IsFizz(int number) => number % 3 == 0;
     private bool IsBuzz(int number) => number % 5 == 0;
-
+    private bool IsFizzBuzz(int number) => IsFizz(number) && IsBuzz(number);
 }
